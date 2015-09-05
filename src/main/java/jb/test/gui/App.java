c7a3bebc29caa58b472e9c2d6fb830d9c9c32221
d-kozhevnikov <dimakozhevnikov+github@gmail.com>
2015-09-05 18:31:12 +0300

    Fix GUI for unknown content-length

--- src/main/java/jb/test/gui/App.java
+++ src/main/java/jb/test/gui/App.java
@@ -61,13 +61,6 @@ public class App {
             SwingUtilities.invokeLater(() -> {
                 Font f = urlLabel.getFont();
                 urlLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
-
-                progressBar.setMinimum(0);
-                if (length.isPresent())
-                    progressBar.setMaximum((int) (length.get() / 1024));
-                else
-                    progressBar.setIndeterminate(true);
-
                 updateProgressBar(false, null);
             });
         }
@@ -90,22 +83,39 @@ public class App {
             SwingUtilities.invokeLater(() -> updateProgressBar(false, null));
         }
 
+        @Override
+        public void onSuccess() throws IOException {
+            super.onSuccess();
+            length = Optional.of(getWrittenLength());
+            SwingUtilities.invokeLater(() -> updateProgressBar(true, null));
+        }
+
         private void processError(Throwable e) {
             e.printStackTrace();
             SwingUtilities.invokeLater(() -> updateProgressBar(false, e));
         }
 
         private void updateProgressBar(boolean done, Throwable error) {
-            progressBar.setValue((int) (getWrittenLength() / 1024));
+            boolean indeterminate = !done && error == null && !length.isPresent();
+            if (indeterminate) {
+                progressBar.setMaximum(0);
+                progressBar.setValue(0);
+            }
+            else {
+                progressBar.setMaximum((int) (length.orElse(0L) / 1024));
+                progressBar.setValue((int) (getWrittenLength() / 1024));
+            }
+            // Doesn't show string on OSX
+            //progressBar.setIndeterminate(indeterminate);
+            progressBar.setStringPainted(true);
 
             if (done)
-                progressBar.setString("Done!");
+                progressBar.setString(String.format("Done! (%d KB)", getWrittenLength() / 1024));
             else if (error != null)
                 progressBar.setString("Error: " + error);
             else
                 progressBar.setString(getProgressString(getWrittenLength(), length));
 
-            progressBar.setIndeterminate(!done && error != null && !length.isPresent());
             updateTotalProgressBar();
         }
     }
@@ -122,11 +132,14 @@ public class App {
 
     private void updateTotalProgressBar() {
         Progress p = downloader.getProgress();
-        totalProgress.setIndeterminate(!p.getTotal().isPresent());
         if (p.getTotal().isPresent()) {
             totalProgress.setMaximum((int) (p.getTotal().get() / 1024));
             totalProgress.setValue((int) (p.getDownloaded() / 1024));
+        } else {
+            totalProgress.setMaximum(0);
+            totalProgress.setValue(0);
         }
+
         totalProgress.setString(getProgressString(p.getDownloaded(), p.getTotal()));
     }
 
