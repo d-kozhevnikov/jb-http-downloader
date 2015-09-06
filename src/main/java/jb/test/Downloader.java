d884c91f87739124cf5c8fbca266058df6801268
d-kozhevnikov <dimakozhevnikov+github@gmail.com>
2015-09-06 20:09:26 +0300

    Add javadoc

--- src/main/java/jb/test/Downloader.java
+++ src/main/java/jb/test/Downloader.java
@@ -3,12 +3,60 @@ package jb.test;
 import java.io.Closeable;
 import java.util.Collection;
 
+
+/**
+ * Downloader is the interface for multithreaded HTTP downloader.
+ *
+ * @author   Dmitriy Kozhevnikov
+ */
 public interface Downloader extends Closeable {
+
+    /**<p>
+     * Downloads URLs specifies via {@code tasks}.
+     * Clients can process downloaded data via callbacks in {@link DownloadingTask}.
+     * </p><p>
+     * This method blocks until finish, but other methods can be called from other threads to
+     * affect the downloading progress and monitor the execution.
+     * </p><p>
+     * This method can be called only once, otherwise {@code IllegalStateException} is thrown
+     * </p>
+     * @param tasks    specifies URL to download and provides callbacks to
+     *                 notify the client about downloading process.
+     *                 @see DownloadingTask
+     *
+     * @param nThreads    initial downloading threads count.
+     *                    @see #setThreadsCount(int)
+     *
+     * @throws InterruptedException if any thread has interrupted the current thread
+     * @throws IllegalStateException if called more that once
+     */
     void run(Collection<? extends DownloadingTask> tasks, int nThreads) throws InterruptedException;
 
+    /**<p>
+     * Reports current downloading progress.
+     * </p><p>
+     * No global synchronization is performed for performance reasons,
+     * so returned data may be slightly obsolete as downloading continues
+     * as total progress is calculated.
+     * </p><p>
+     * Return valid is guaranteed to be actual after the downloading is complete.
+     * </p>
+     * @return total downloading progress (sum of downloaded data and sum of
+     * all file lengths if known)
+     */
     Progress getProgress();
 
+
+    /**
+     * Changes downloading threads count.
+     *
+     * @param nThreads new threads count
+     */
     void setThreadsCount(int nThreads);
 
+    /**
+     * Stops all downloading and closes all connections. Does nothing if downloading already finished.
+     * Blocks till everything is stopped.
+     */
     void close();
 }
--- src/main/java/jb/test/DownloadingTask.java
+++ src/main/java/jb/test/DownloadingTask.java
@@ -2,22 +2,73 @@ package jb.test;
 
 import java.io.IOException;
 import java.net.URL;
-import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.Optional;
 
+/**<p>
+ * Specifies URL to download and provides callback to process downloaded data.
+ * All callbacks can be called from arbitrary threads,
+ * but no two callbacks are called synchronously.
+ * </p><p>
+ * <b>Error handling strategy:</b> any errors reported by callbacks
+ * are passed to {@link #onFailure(Throwable)} and no further
+ * downloading is performed.
+ * </p>
+ *
+ * @author Dmitriy Kozhevnikov
+ */
 public interface DownloadingTask {
+    /**
+     * @return URL to download
+     */
     URL getURL();
 
+    /**
+     * Is called exactly once before actual downloading begins.
+     *
+     * @param contentLength expected content length,
+     *                      can be {@code empty} if server didn't send Content-Length
+     * @throws IOException if any error is occurred (see "Error handling strategy")
+     */
     void onStart(Optional<Long> contentLength) throws IOException;
 
+    /**
+     * Is called when the next data chunk is ready for processing. Subsequent calls represent
+     * adjacent chunks, chunk in the very first call is the chunk with zero offset.
+     *
+     * @param chunk read-only data chunk
+     * @throws IOException if any error is occurred (see "Error handling strategy")
+     */
     void onChunkReceived(ByteBuffer chunk) throws IOException;
 
+    /**
+     * Is called when the downloading is finished.
+     * 
+     * @throws IOException if any error is occurred (see "Error handling strategy")
+     */
     void onSuccess() throws IOException;
 
+    /**
+     * Is called when the task is cancelled due to decrease of the thread count.
+     * Client should drop all processed data, the following
+     * {@link #onChunkReceived(ByteBuffer)} would represent the chunk with zero offset again
+     *
+     * @throws IOException if any error is occurred (see "Error handling strategy")
+     */
     void onCancel() throws IOException;
 
+    /**
+     * Is called when downloading is failed because of any reason (including
+     * exception in any other callback). No further downloading will be performed. 
+     * 
+     * @param cause an exception caused the failure.
+     */
     void onFailure(Throwable cause);
 
+    /**
+     * Is called when downloading is interrupted because of downloader shutdown or other client
+     * actions. No further downloading will be performed.
+     * @throws IOException if any error is occurred (see "Error handling strategy")
+     */
     void onDiscard() throws IOException;
 }
